383c92ac9fcd9408437df31cd096e85c6874536c
admin <admin@DESKTOP-J3L35AE>
2023-08-12 11:57:59 +0900

    주문/배송 일부

--- crueltyfree/src/main/java/com/crfr/controller/OrderDeliveryController.java
+++ crueltyfree/src/main/java/com/crfr/controller/OrderDeliveryController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.crfr.service.orderDelivery.OrderDeliveryService;
 import com.crfr.service.productView.ProductViewService;
 import com.crfr.vo.FileVo;
 import com.crfr.vo.MemberVo;
@@ -30,6 +31,9 @@ public class OrderDeliveryController {
 	@Setter(onMethod_={ @Autowired })	
 	ProductViewService pSelectView, pSelectThumbnail;
 	
+	@Setter(onMethod_={ @Autowired })	
+	OrderDeliveryService mSelectOrderVo, mSelectOrderProduct;
+	
 	@GetMapping("/mypage_orderinq.do")
 	public String mypage_orderinq(Model model, String pageNum, String pageBlock, 
 			HttpServletRequest request) {
@@ -42,34 +46,36 @@ public class OrderDeliveryController {
 		}
 		
 		List<OrderListVo> orderList = new ArrayList<>(); // 빈 리스트로 초기화
+		List<OrderVo> orderVoList = mSelectOrderVo.selectOrderVo(member_idx);//주문리스트 받아오기
 		
-//		List<OrderVo> orderVoList = mSelectOrderVo.selectOrderVo(member_idx);//주문리스트 받아오기
-//		
-//		for(OrderVo orderVo : orderVoList) {
-//			int order_idx = orderVo.getOrder_idx();
-//					
-//			List<OrderProductVo> orderProductList = mSelectOrderProduct.selectOrderProduct(order_idx);
-//			for(OrderProductVo orderProductVo : orderProductList) {
-//				int product_idx = orderProductVo.getProduct_idx();
-//				ProductVo productVo = pSelectView.selectView(product_idx); //상품번호의 상품Vo
-//			    FileVo fileVo = pSelectThumbnail.selectThumbnail(product_idx); //상품번호의 파일Vo중 1번째
-//			    
-//			    OrderProductVo orderProductVo = mSelectOrderProduct.selectOrderProduct(order_idx, product_idx);
-//			    
-//			    OrderListVo orderListVo = new OrderListVo(); //OrderListVo 객체 생성
-//			    //주문idx,날짜,번호,상품썸네일,상품판매자,상품이름,구매개수,상품구매금액,주문진행상태
-//			    orderListVo.setOrder_idx(order_idx);
-//			    orderListVo.setOrder_date(orderVo.getOrder_date());
-//			    orderListVo.setOriginFile(fileVo.getOriginFile());
-//			    orderListVo.setSaveFile(fileVo.getSaveFile());
-//			    orderListVo.setMember_nickname(productVo.getMember_nickname());
-//			    orderListVo.setProduct_name(productVo.getProduct_name());
-//			    orderListVo.setProduct_price(productVo.getProduct_price());
-//			    orderListVo.setOrder_product_count(orderProductVo.getOrder_product_count());
-//			    orderListVo.setOrder_status(orderVo.getOrder_status());
-//			}			
-//		}
+		for(OrderVo orderVo : orderVoList) {
+			int order_idx = orderVo.getOrder_idx();
+					
+			List<OrderProductVo> orderProductList = mSelectOrderProduct.selectOrderProduct(order_idx);
+			for(OrderProductVo orderProductVo : orderProductList) {
+				int product_idx = orderProductVo.getProduct_idx();
+				ProductVo productVo = pSelectView.selectView(product_idx); //상품번호의 상품Vo
+			    FileVo fileVo = pSelectThumbnail.selectThumbnail(product_idx); //상품번호의 파일Vo중 1번째
+			    			    
+			    OrderListVo orderListVo = new OrderListVo(); //OrderListVo 객체 생성
+			    //주문idx,날짜,번호,상품썸네일,상품판매자,상품이름,구매개수,상품구매금액,주문진행상태
+			    orderListVo.setOrder_idx(order_idx);
+			    orderListVo.setOrder_date(orderVo.getOrder_date());
+			    orderListVo.setOrder_num(orderVo.getOrder_num());
+			    orderListVo.setOriginFile(fileVo.getOriginFile());
+			    orderListVo.setSaveFile(fileVo.getSaveFile());
+			    orderListVo.setMember_nickname(productVo.getMember_nickname());
+			    orderListVo.setProduct_name(productVo.getProduct_name());
+			    orderListVo.setProduct_price(productVo.getProduct_price());
+			    orderListVo.setOrder_product_count(orderProductVo.getOrder_product_count());
+			    orderListVo.setOrder_status(orderVo.getOrder_status());
+			    orderListVo.setProduct_idx(product_idx);
+			    
+			    orderList.add(orderListVo); // basketList에 추가
+			}//주문상품 for문 종료
+		}//주문테이블vo for문 종료
 		
+		model.addAttribute("orderList", orderList); // basketList를 모델에 추가	
 		return "mypage/mypage_orderinq";
 	}
 	
